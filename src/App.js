e2dfe100af7587c93427b42c2ab49c2ae205a151
Brian Murray <brianmurraysound@gmail.com>
2021-08-10 12:52:26 -0300

    song data setting state and playback function implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 // import styles
 import "./styles/app.scss";
+// import util
+import data from "./util";
 //import components
 import Player from "./components/Player";
 import Song from "./components/Song";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
